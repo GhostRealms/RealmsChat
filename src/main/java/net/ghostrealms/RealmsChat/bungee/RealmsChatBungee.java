2747c9ba45518814801bda2a0811be3249b5e241
River Marmorstein <river@rmarmorstein.com>
2016-08-17 14:13:53 -0400

    starting a boilerplate bungee code

---
+++ src/main/java/net/ghostrealms/RealmsChat/bungee/RealmsChatBungee.java
@@ -0,0 +1,20 @@
+package net.ghostrealms.RealmsChat.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * Created by River on 8/17/2016.
+ */
+public class RealmsChatBungee extends Plugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
